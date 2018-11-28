982434013ba87d6b3ab5ab985fd18173a950fa92
James Mark <noemail@jamesmark.net>
2018-11-27 21:51:13 -0600

    Set up basics in bootstrap, needs testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-# TODO: Remove any existing conflicting files
+# Remove any existing conflicting files
 
-# TODO: Symlink files into place
+if [ -f ~/.bash_profile ]
+then
+	mv .bash_profile .bash_profile.bkp
+fi
+
+if [ -f ~/.aliases ]
+then
+	mv .aliases .aliases.bkp
+fi
+
+if [ -f ~/.functions ]
+then
+	mv .functions .functions.bkp
+fi
+
+if [ -f ~/.tmux ]
+then
+	mv .tmux .tmux.bkp
+fi
+
+if [ -f ~/.vimrc ]
+then
+	mv .vimrc .vimrc.bkp
+fi
+
+# Symlink files into place
+
+ln -s bash_profile ~/.bash_profile
+ln -s aliases ~/.aliases
+ln -s functions ~/.functions
+ln -s vimrc ~/.vimrc
